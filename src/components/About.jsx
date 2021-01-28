c9a49e04b36b343c600d8f21b1c137e89582d032
Emine <76777299+eminekbl@users.noreply.github.com>
2021-01-28 16:54:42 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About(props) {
             <Row className="justify-content-center">
                 <Col sm={5} id='block'>
                 <h1>ABOUT THIS PROJECT</h1>
-                <p>You can find the photos you are interested in by searching keywords. You can find the photographer's name, biography if available, and a photo description. You can also check out the photographer's Unsplash portfolio.
+                <p>Find the photos you are interested in by searching keywords. You can find the photographer's name, biography if available, and a photo description. You can also check out the photographer's Unsplash portfolio.
 This website uses unsplash-api, be sure to change API Key before cloning.</p>
                 <a id="license" href="https://unsplash.com/license">
                 Read More About Unsplash License </a>
