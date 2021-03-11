a18b6bc30ba5cd7c960249dd0585567e16e3d4cd
Emine Kebeli <76777299+eminekbl@users.noreply.github.com>
2021-03-11 23:01:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ if (process.env.NODE_ENV === "production") {
     app.get('/resetpassword/:resetToken', function (req, res) {
         res.sendFile(path.join(__dirname, "client", "build", index.html))
     });
+    app.get('/search/:query', function (req, res) {
+        res.sendFile(path.join(__dirname, "client", "build", index.html))
+    });
 
 }
 
