8bf0b20db37a466d2d9993e65c4a65f49039af36
Emine Kebeli <76777299+eminekbl@users.noreply.github.com>
2021-03-11 22:02:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use('/api/private', require('./routers/private'))
 //     res.send("HELLO FROM ROOOOT")
 // })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", index.html))
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, "client", "build", index.html))
+//     })
+// }
 
 
 
